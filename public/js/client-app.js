d930e2661cd2982b52e85022664fab16a0ae8aca
Pratham Sapkota <prathamsapkota1837@gmail.com>
2022-05-13 19:52:17 +0545

    Delete unnecessary files

--- public/js/client-app.js
+++ public/js/client-app.js
@@ -1,20 +1,4 @@
-// console.log("browser rendering")
 
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//         response.json().then((data)=>{
-//             console.log(data)
-//         })
-// })
-// fetch('http://localhost:3000/weather?address=Kathmandu').then((response)=>{
-//     response.json().then((data)=>{
-//         if(data.error){
-//             console.log(data.error)
-//         }else{
-//             console.log(data.location)
-//             console.log(data.forecast)
-//         }
-//     })
-// })
 
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
